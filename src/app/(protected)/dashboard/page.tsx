a91898b949f7a627147b31a936241b97f10cc784
Haroon Hakimi <haroonhakimi2005@gmail.com>
2024-12-25 19:07:14 -0500

    initial start

---
+++ src/app/(protected)/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+type Props = {};
+
+const Page = (props: Props) => {
+  // Server Action to Onboard the user
+  
+  return <div>Page</div>;
+};
+
+export default Page;
